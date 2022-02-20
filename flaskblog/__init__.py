36647efe41d9a04c8e1fc380137ea1e7496b54cf
Olamide643 <71270347+Olamide643@users.noreply.github.com>
2022-02-20 22:32:15 +0100

    Update Init

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -21,7 +21,7 @@ def Logger():
 logger = Logger()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
