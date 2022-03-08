14b33472ddd126cf64e614829991e69184cfc5b1
Olamide643 <71270347+Olamide643@users.noreply.github.com>
2022-03-08 05:35:08 +0100

    Upload image to cloudinary

--- flaskblog/models/UserModel.py
+++ flaskblog/models/UserModel.py
@@ -15,7 +15,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, 
+    image_file = db.Column(db.String(500), nullable=False, 
                            default='https://res.cloudinary.com/teamblog/image/upload/v1646713752/default_xfjzu9.jpg')
     password = db.Column(db.String(60), nullable=False)
     verified = db.Column(db.Integer, nullable = False, default = 0)
